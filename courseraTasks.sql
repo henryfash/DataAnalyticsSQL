2c45d443927d2c7aaf4d28bdb76dcd71dff020bb
henryfash <44878078+henryfash@users.noreply.github.com>
2019-09-04 13:00:53 +0200

    Added task 4 and removed task 1

--- courseraTasks.sql
+++ courseraTasks.sql
@@ -1,63 +1,5 @@
---Task 1
-
--- remove any table with the name INSTRUCTOR
-
-DROP TABLE INSTRUCTOR
-;
-
---Creates a table INSTRUCTOR
-CREATE TABLE INSTRUCTOR (
-ins_id INTEGER PRIMARY KEY NOT NULL,
-lastname VARCHAR(15) NOT NULL,
-firstname VARCHAR(15) NOT NULL,
-city VARCHAR(15),
-country char(2)
-)
-;
-
---Insert one row into the table 
-INSERT INTO INSTRUCTOR 
-(ins_id, lastname, firstname, city, country)
-VALUES
-(1, 'Ahuja', 'Rav', 'Toronto', 'CA')
-
-;
-
---inserting multiple rows at the same time
-INSERT into INSTRUCTOR 
-(ins_id, lastname, firstname, city, country)
-VALUES
-(2, 'Chong', 'Raul', 'Toronto', 'CA'),
-(3, 'Vasudevan', 'Hima', 'Chicago', 'US')
-;
-
---Query the table to get all fields
-select * from INSTRUCTOR;
-
---Query just to obtain the firstname, lastname and country
-SELECT firstname, lastname, country from INSTRUCTOR 
-WHERE city = 'Toronto';
-
---Update one row
-UPDATE INSTRUCTOR SET
-city = 'Markham' WHERE ins_id =1 
-;
-
-select city from INSTRUCTOR
-where ins_id = 1 
-;
-
---Delete a row
-DELETE FROM INSTRUCTOR
-WHERE ins_id = 2 
-;
-
---Query to obtain all fields
-select * from INSTRUCTOR;
-
 -- Task 2 
-
-------------------------------------------
+------------------------------------
 --DDL statement for table 'HR' database--
 --------------------------------------------
 
@@ -163,3 +105,84 @@ from EMPLOYEES as E, DEPARTMENTS as D
 where E.DEP_ID = D.DEPT_ID_DEP
 order by D.DEP_NAME, E.L_NAME desc ;
 
+
+--Task 4
+
+--Testing 
+
+SELECT * FROM EMPLOYEES;
+SELECT * FROM DEPARTMENTS ;
+SELECT * FROM JOB_HISTORY ;
+SELECT * FROM JOBS ;
+
+
+
+/* Query 1A: Select the names and job start dates of all employees who work for
+ the department number 5. */
+
+SELECT E.F_NAME, E.L_NAME, JH.START_DATE FROM 
+		EMPLOYEES AS E INNER JOIN JOB_HISTORY AS JH
+		ON E.EMP_ID = JH.EMPL_ID
+		WHERE DEP_ID =5
+		;
+
+/* Query 1B: Select the names, job start dates, and job titles of all employees  
+who work for the department number ... From 3 tables: EMPLOYEES, JOB_HISTORY; JOBS*/
+
+SELECT E.F_NAME, E.L_NAME, JH.START_DATE, J.JOB_TITLE FROM 
+		EMPLOYEES AS E 
+		INNER JOIN JOB_HISTORY AS JH ON E.EMP_ID = JH.EMPL_ID
+		INNER JOIN JOBS AS J ON JH.JOBS_ID = J.JOB_IDENT
+		
+		WHERE DEP_ID =5
+		;
+
+/*Query 2A: Perform a Left Outer Join on the EMPLOYEES and DEPARTMENT
+tables and select employee id, last name, department id and department name
+for all employees */
+
+SELECT E.EMP_ID, E.L_NAME, D.DEPT_ID_DEP, D.DEP_NAME FROM 
+		EMPLOYEES AS E
+		LEFT OUTER JOIN DEPARTMENTS AS D 
+		ON  E.DEP_ID = D.DEPT_ID_DEP
+		;
+
+/* Query 2B: Re-write the query for 2A to limit the result set to include only the
+rows for employees born before 1980.  */
+
+SELECT E.EMP_ID, E.L_NAME, D.DEPT_ID_DEP, D.DEP_NAME FROM 
+		EMPLOYEES AS E
+		LEFT OUTER JOIN DEPARTMENTS AS D 
+		ON  E.DEP_ID = D.DEPT_ID_DEP
+		WHERE  YEAR(E.B_DATE) < 1980
+		;
+
+/*Query 2C: Re-write the query for 2A to have the result set include all the
+employees but department names for only the employees who were born
+before 1980.*/
+
+select E.EMP_ID,E.L_NAME,E.DEP_ID,D.DEP_NAME
+	from EMPLOYEES AS E 
+	LEFT OUTER JOIN DEPARTMENTS AS D ON E.DEP_ID=D.DEPT_ID_DEP 
+	AND YEAR(E.B_DATE) < 1980
+;
+/*Query 3A: Perform a Full Join on the EMPLOYEES and DEPARTMENT tables
+and select the First name, Last name and Department name of all employees.*/
+
+select E.F_NAME,E.L_NAME,D.DEP_NAME
+	from EMPLOYEES AS E 
+	FULL OUTER JOIN DEPARTMENTS AS D ON E.DEP_ID=D.DEPT_ID_DEP
+;
+
+/* Query 3B: Re-write Query 3A to have the result set include all employee
+names but department id and department names only for male employees.*/
+
+select E.F_NAME,E.L_NAME,D.DEPT_ID_DEP, D.DEP_NAME
+	from EMPLOYEES AS E 
+	FULL OUTER JOIN DEPARTMENTS AS D ON E.DEP_ID=D.DEPT_ID_DEP AND E.SEX = 'M'
+;
+--- alt Query 3B ---
+select E.F_NAME,E.L_NAME,D.DEPT_ID_DEP, D.DEP_NAME
+	from EMPLOYEES AS E 
+	LEFT OUTER JOIN DEPARTMENTS AS D ON E.DEP_ID=D.DEPT_ID_DEP AND E.SEX = 'M'
+;
