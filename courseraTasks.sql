99315d06e2a43ff94efa3f99b5e4efff68cdcbec
henryfash <44878078+henryfash@users.noreply.github.com>
2019-09-04 13:20:58 +0200

    added task 1 to master

--- courseraTasks.sql
+++ courseraTasks.sql
@@ -1,3 +1,44 @@
+--Task 1
+
+-- remove any table with the name INSTRUCTOR
+
+DROP TABLE INSTRUCTOR
+;
+
+--Creates a table INSTRUCTOR
+CREATE TABLE INSTRUCTOR (
+ins_id INTEGER PRIMARY KEY NOT NULL,
+lastname VARCHAR(15) NOT NULL,
+firstname VARCHAR(15) NOT NULL,
+city VARCHAR(15),
+country char(2)
+)
+;
+
+--Insert one row into the table 
+INSERT INTO INSTRUCTOR 
+(ins_id, lastname, firstname, city, country)
+VALUES
+(1, 'Ahuja', 'Rav', 'Toronto', 'CA')
+
+;
+
+--inserting multiple rows at the same time
+INSERT into INSTRUCTOR 
+(ins_id, lastname, firstname, city, country)
+VALUES
+(2, 'Chong', 'Raul', 'Toronto', 'CA'),
+(3, 'Vasudevan', 'Hima', 'Chicago', 'US')
+;
+
+--Query the table to get all fields
+select * from INSTRUCTOR;
+
+--Query just to obtain the firstname, lastname and country
+SELECT firstname, lastname, country from INSTRUCTOR 
+WHERE city = 'Toronto';
+
+
 -- Task 2 
 ------------------------------------
 --DDL statement for table 'HR' database--
